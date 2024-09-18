299174122f5a99bfb6b0a661e42ce229517b1f2c
Sakthiganesh <sakthiganesh782@gmail.com>
2024-09-18 03:53:33 -0700

    Initial commit

---
+++ src/utils/index.js
@@ -0,0 +1,46 @@
+export const navOptions = [
+    {
+        id: "home",
+        label: "Home",
+        path: "/",
+
+    },
+    {
+        id: "listing",
+        label: "All Products",
+        path: "/product/listing/all-product",
+
+    },
+    {
+        id: "listingMen",
+        label: "Men",
+        path: "/product/Listing/men",
+
+    },
+    {
+        id: "listingWomen",
+        label: "Women",
+        path: "/product/Listing/women",
+
+    },
+    {
+        id: "listingKids",
+        label : "kids",
+        path: "/product/listing/kids",
+
+    },
+];
+
+export const adminNavOptions = [
+    {
+        id: "adminListing",
+        label: "Manage All Products",
+        path: "/admin-view/all-product",
+    },
+    {
+        id: "adminNewProduct",
+        label: "Add New Products",
+        path: "/admin-view/add-product",
+    },
+ 
+];
